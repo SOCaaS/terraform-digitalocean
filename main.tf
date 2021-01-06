7c288280e726cb24e51eb0288c8ef73d60493a5d
Ivan Ezechial Suratno <32078894+ezeutno@users.noreply.github.com>
2021-01-06 18:45:41 +0700

    Remove the server

--- main.tf
+++ main.tf
@@ -8,9 +8,13 @@ module "digitalocean" {
     source              = "./droplet"
     servers             = [
 #         {
-#             name = "server-test",
+#             name = "server-1",
 #             type = "s-1vcpu-1gb"
 #         }
-#     ]
+#         ,{
+#             name = "server-2",
+#             type = "s-1vcpu-1gb"
+#         }
+    ]
     digital_ocean_key   = var.digital_ocean_key
 }
