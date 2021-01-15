b4fa0cac5e370f9c6360efd2456e4a61d28da45e
GeorgiusKurli <32050591+GeorgiusKurli@users.noreply.github.com>
2021-01-15 15:42:35 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,14 +7,14 @@ terraform {
 module "digitalocean" {
     source              = "./droplet"
     servers             = [
-#         {
-#             name = "server-1",
-#             type = "s-1vcpu-1gb"
-#         }
-#         ,{
-#             name = "server-2",
-#             type = "s-1vcpu-1gb"
-#         }
+        {
+            name = "server-1",
+            type = "s-1vcpu-1gb"
+        },
+        {
+            name = "server-2",
+            type = "s-1vcpu-1gb"
+        }
     ]
     digital_ocean_key   = var.digital_ocean_key
 }
